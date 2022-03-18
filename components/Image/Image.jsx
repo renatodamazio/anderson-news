e75c359054b91b2947702bfbe4f28bc650754873
Renato <renatopdamazio@gmail.com>
2022-03-18 16:14:03 -0300

    Image component with placeholder

--- components/Image/Image.jsx
+++ components/Image/Image.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export const Image = (props) => {
-  return <LazyLoadImage effect="opacity" {...props} />;
+  return (
+    <LazyLoadImage
+      effect="opacity"
+      placeholderSrc="/images/default.jpeg"
+      visibleByDefault={props.src === "/images/default.jpeg"}
+      {...props}
+    />
+  );
 };
 
 export default Image;
